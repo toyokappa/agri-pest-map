cae68585e6664dce1be69dd010ea1bce464a38ce
Yuta Toyokawa <kppg42@gmail.com>
2022-05-19 23:57:29 +0900

    basic認証を試しに追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    'nuxt-basic-auth-module'
   ],
+  basic: {
+    name: 'user',
+    pass: 'pass',
+    enabled: true
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
